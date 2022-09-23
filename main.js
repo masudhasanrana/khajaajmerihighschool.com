07c7bb1e96531dd5e0f7031ed502e3c69e3f8626
masudhasanrana <110655086+masudhasanrana@users.noreply.github.com>
2022-09-23 21:55:33 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// change navbar style on scroll //
+window.addEventListener('scroll',()=> {
+    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+})
+
+//show the qas answer///
+const qas = document.querySelectorAll('.qa');
+
+qas.forEach (qa => {
+    qa.addEventListener('click', () => {
+        qa.classList.toggle('open');
+
+        /////////icon change////////
+        const icon = qa.querySelector('.qa__icon i');
+        if(icon.className ==='uil uil-plus'){
+            icon.className = 'uil uil-minus'
+        }
+        else{
+            icon.className = 'uil uil-plus'
+        }
+    })
+})
+
+//Show nav bar///
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display= 'none';
+})
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display= 'inline-block';
+}
+closeBtn.addEventListener('click' , closeNav)
